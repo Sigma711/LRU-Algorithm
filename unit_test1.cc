e3a55a9cb99180ba057cd4fea68a88e9fe2bfaec
Sigma711 <1979934715@qq.com>
2021-02-26 00:03:40 +0000

    complete one unit test example

--- unit_test1.cc
+++ unit_test1.cc
@@ -7,6 +7,18 @@
 // execute unit tests
 int main() {
 
+	TEST_LRU_CLASS(std::vector<TUPLE>(
+			{TUPLE({'g', 3, ""}), TUPLE({'p', 3, "ABC"}), TUPLE({'g', 3, ""}), TUPLE({'p', 4, "ABCD"}),
+			 TUPLE({'p', 6, "ABCDEF"}), TUPLE({'p', 3, "CBA"})}));
+	TEST_LRU_CLASS(std::vector<TUPLE>(
+			{TUPLE({'g', 3, ""}), TUPLE({'p', 3, "ABC"}), TUPLE({'g', 3, ""}), TUPLE({'p', 4, "ABCD"}),
+			 TUPLE({'p', 6, "ABCDEF"}), TUPLE({'p', 3, "CBA"}), TUPLE({'p', 5, "ABCDE"}), TUPLE({'p', 1, "A"}),
+			 TUPLE({'p', 6, "FEDCBA"}), TUPLE({'p', 8, "ABCDEFGH"})}));
+	TEST_LRU_CLASS(std::vector<TUPLE>(
+			{TUPLE({'g', 3, ""}), TUPLE({'p', 3, "ABC"}), TUPLE({'g', 3, ""}), TUPLE({'p', 4, "ABCD"}),
+			 TUPLE({'p', 6, "ABCDEF"}), TUPLE({'p', 3, "CBA"}), TUPLE({'p', 5, "ABCDE"}), TUPLE({'p', 1, "A"}),
+			 TUPLE({'p', 6, "FEDCBA"}), TUPLE({'p', 8, "ABCDEFGH"}), TUPLE({'p', 4, "DCBA"}), TUPLE({'g', 5, ""})}));
+
 	exit(0);
 
 }
